ff19173556a01389854f7243af6b9585d57d9422
kzrnm <gengesa@gmail.com>
2021-11-13 13:42:53 +0900

    Add AggressiveInlining

--- Base32768/Utils.cs
+++ Base32768/Utils.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using System.Text;
 
 #if !NETCOREAPP3_0_OR_GREATER
 namespace System.Runtime.CompilerServices
