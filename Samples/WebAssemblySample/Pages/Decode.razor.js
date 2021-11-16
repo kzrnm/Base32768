4c8966f7efdfb3c9755a44c1040d136603727884
kzrnm <gengesa@gmail.com>
2021-11-16 23:45:52 +0900

    WebAssembly

---
+++ Samples/WebAssemblySample/Pages/Decode.razor.js
@@ -0,0 +1,20 @@
+﻿
+function b64toBlob(base64, type = 'application/octet-stream') {
+    return fetch(`data:${type};base64,${base64}`).then(res => res.blob())
+}
+
+async function BlazorDownloadFile(filename, contentType, content) {
+    const data = await b64toBlob(content, contentType);
+
+    const file = new File([data], filename, { type: contentType });
+    const exportUrl = URL.createObjectURL(file);
+
+    const a = document.createElement("a");
+    document.body.appendChild(a);
+    a.href = exportUrl;
+    a.download = filename;
+    a.target = "_self";
+    a.click();
+
+    URL.revokeObjectURL(exportUrl);
+}
